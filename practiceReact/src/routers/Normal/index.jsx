609ad60d4586016c3465df2c7aa123dffbf84b24
Kalal mukesh <105076417+Mukeshkalal@users.noreply.github.com>
2024-02-08 20:19:18 +0530

    blog pages add

--- practiceReact/src/routers/Normal/index.jsx
+++ practiceReact/src/routers/Normal/index.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header";
 import About from "../../pages/About"
 import Home from "../../pages/Home"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Blog from "../../pages/Blog";
 function Normal() {
     return (
         <BrowserRouter>
@@ -11,6 +12,7 @@ function Normal() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/blog" element={<Blog />} />
                 </Routes>
             </Container>
         </BrowserRouter>
